9482fd5348bedf1dd2487ed9b04153e4bf1ee2e6
berfin7a <114851768+berfin7a@users.noreply.github.com>
2023-07-26 11:42:56 +0300

    Create Fighter.java

---
+++ Fighter.java
@@ -0,0 +1,24 @@
+package java101;
+
+public class Fighter {
+    String name;
+    int damage;
+    int health;
+    int weight;
+
+    Fighter(String name, int damage,int health,int weight){
+        this.name =name;
+        this.damage =damage;
+        this.health =health;
+        this.weight =weight;
+    }
+
+    int hit(Fighter foe){
+        System.out.println(this.name +" =>" + foe.name + " " + this.damage + "hasar vrudu");
+        if(foe.health - this.damage < 0){
+            return 0;
+        }
+        return foe.health - this.damage;
+    }
+
+}
